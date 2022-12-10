9480de88a5a018f67c007906aede3ff5ea0fac21
Alex <eddings.sharanna@gmail.com>
2022-12-09 19:58:35 -0500

    imported components and added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 // route and components
 import { Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Main from "./pages/Main";
+import HomePage from "./pages/HomePage";
+import EmployeePage from "./pages/EmployeePage";
 
 // css
 import "./App.css";
@@ -9,6 +13,12 @@ export default function App() {
   return (
     <div className="App">
       <h1>Darkplace Hospital</h1>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/employeepage" element={<EmployeePage />} />
+      </Routes>
     </div>
   );
 }
