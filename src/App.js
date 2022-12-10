d2b127ea6d1a7ecfabe31d9b6facd478b624a2c9
Alex <eddings.sharanna@gmail.com>
2022-12-09 19:33:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// route and components
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+// css
+import "./App.css";
+
+export default function App() {
+  // use route component to specify each route
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Darkplace Hospital</h1>
     </div>
   );
 }
-
-export default App;
